44be844b5aaebb5ea52249b225c0f58d53a93a9f
unknown <andrei.bacaila15@gmail.com>
2025-07-15 21:22:42 +0300

    create navbar and first component

--- src/pages/Home.vue
+++ src/pages/Home.vue
@@ -1,7 +1,77 @@
+<script lang="ts" setup>
+  import phone from "../assets/phone.png";
+  import laptop from "../assets/laptop.png";
+  import camera from "../assets/camera.png";
+  import headset from "../assets/headset.png";
+  import tv from "../assets/TV.png";
+</script>
 <template>
-    <div>
-        <h1>Welcome to the Home Page</h1>
-        <p>This is a simple Vue.js application.</p>
-        <router-link to="/about">Go to About Page</router-link>
+    <div class="wrapper">
+        <div class="third">
+             <div class="container">
+                  <img class="image" :src="phone" alt="phone" />
+                  <img class="image" :src="phone" alt="phone" />
+                  <img class="image" :src="phone" alt="phone" />
+             </div>
+        </div>
+        <div class="second"></div>
+        <div class="first"></div>
     </div>
 </template>
+
+
+<style scoped>
+.wrapper {
+    background-color: brown;
+    width: 100%;
+    min-height: 75%;
+    margin-top: 6rem;
+    display: flex;
+    justify-content: center;
+    position: relative;
+}
+
+.third {
+    background-color: blue;
+    width: 95%;
+    height: 70vh;
+    border-radius: 40px;
+    display: flex;
+    justify-content: end;
+}
+.container{
+    height: 200px;
+    width: 50px;
+    background-color: orange;
+    margin-right: 1rem;
+    margin-top: 4rem;
+    border-radius: 10px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    gap: 1rem;
+    align-items: center;
+}
+.image{
+    width: 2.5rem;
+    height: 2.5rem;
+}
+
+.second {
+    position: absolute;
+    background-color: yellow;
+    width: 200px;
+    height: 200px;
+    border-radius: 50%;
+    bottom: -95px;
+}
+
+.first {
+    position: absolute;
+    background-color: green;
+    width: 120px;
+    height: 120px;
+    border-radius: 50%;
+    bottom: -55px;
+}
+</style>
